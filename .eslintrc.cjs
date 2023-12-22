98973014b82c2c39e7acda764c19f56a0361947e
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-23 00:45:12 +0500

    fix: eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,79 @@
 module.exports = {
-    root: true,
-    env: {browser: true, es2020: true},
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        'react-refresh',
-        'prettier',
-        'import'
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'import'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
+    'prettier/prettier': 'error',
+    semi: ['error', 'never'],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'],
+          'index',
+          'object',
+          'type',
+          'unknown',
         ],
-        'prettier/prettier': 'error',
-        'semi': ['error', 'never'],
-        'import/order': ['error', {
-            'groups': ['builtin', 'external', 'internal', ['sibling', 'parent']],
-            'newlines-between': 'always',
-            'alphabetize': {
-                order: 'asc',
-                caseInsensitive: true,
-            },
-        }],
-        'padding-line-between-statements': [
-            'error',
-            { blankLine: 'always', prev: 'import', next: '*' },
-            { blankLine: 'any', prev: 'import', next: 'import' },
-            { blankLine: 'always', prev: '*', next: 'export' },
-            { blankLine: 'any', prev: 'export', next: 'export' }
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'antd',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '**/*.module.scss',
+            group: 'unknown',
+            position: 'after',
+          },
+          {
+            pattern: '**/*.tsx',
+            group: 'parent',
+            position: 'after',
+          },
         ],
-
-    },
-    settings: {
-        'import/resolver': {
-            // Указываем ESLint использовать TypeScript для разрешения алиасов
-            typescript: {},
+        pathGroupsExcludedImportTypes: ['react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
         },
+      },
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: 'import', next: '*' },
+      { blankLine: 'any', prev: 'import', next: 'import' },
+      { blankLine: 'always', prev: '*', next: 'export' },
+      { blankLine: 'any', prev: 'export', next: 'export' },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      // Указываем ESLint использовать TypeScript для разрешения алиасов
+      typescript: {},
     },
-};
-
+  },
+}
