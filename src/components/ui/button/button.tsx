ee43705ce4b24986cd63f6d6a464b1ac0336a76f
SempaiDarcy <mr_darcy_1@mail.ru>
2023-12-25 03:07:12 +0500

    fix: universal Button component

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,18 +1,37 @@
-import { Button as BaseButton } from 'antd'
+import { FC } from 'react'
+
+import { ButtonSize } from 'antd/es/button'
+
+import { Button as BaseButton, Form } from 'antd'
 
 type ButtonProps = {
   text: string
   onClick?: () => void
   className?: string
   type?: ButtonType
+  size?: ButtonSize
 }
 type ButtonType = 'primary' | 'text' | 'link' | 'default' | 'dashed' | undefined
 
-export const Button = (props: ButtonProps) => {
-  const { text, onClick, className, type } = props
+export const Button: FC<ButtonProps> = ({
+  text,
+  onClick,
+  className,
+  type,
+  size,
+}) => {
   return (
-    <BaseButton type={type} onClick={onClick} className={className}>
-      {text}
-    </BaseButton>
+    <Form.Item>
+      <BaseButton
+        type={type}
+        onClick={onClick}
+        className={className}
+        htmlType={'submit'}
+        block
+        size={size}
+      >
+        {text}
+      </BaseButton>
+    </Form.Item>
   )
 }
