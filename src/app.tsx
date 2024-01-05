a3d029a8ce1d344d030e62ce04d95106ea86b7b4
chat <mr_darcy_1@mail.ru>
2024-01-05 15:55:09 +0600

    feat: add correctly global style

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 import Layout from 'antd/es/layout/layout'
 
 import { SignInForm } from '@/components/sign-in'
 import { SignUpForm } from '@/components/sign-up'
+import { DialogsPage } from '@/pages/dialogs-page/dialogs-page'
 
-import './app.css'
+import s from '@/styles/layout.module.scss'
 
 export const App = () => {
+  const location = useLocation()
+  const isDialogPage = location.pathname === '/me'
   return (
-    <Layout className={'app'}>
+    <Layout className={!isDialogPage ? s.antdLayout : ''}>
       <Routes>
         <Route path={'/'} element={<SignInForm />} />
         <Route path={'/sign-up'} element={<SignUpForm />} />
+        <Route path={'/me'} element={<DialogsPage />} />
       </Routes>
     </Layout>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from '@/app'
 
+import '@/styles/index.scss'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
